8625d380f72c2ce5e5b1c405dc7d5506c761387d
Cao Quang Binh <binhcq@asiantech.vn>
2016-02-17 13:53:02 +0700

    [Update] deploy folder

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,8 +3,7 @@ lock '3.4.0'
 set :application, 'contactbook'
 set :repo_url, 'git@github.com:CQBinh/contact-book.git'
 set :branch, :master
-set :deploy_to, '/home/www.appnhe.com/public_html/contactbook'
-# set :deploy_to, '/home/ubuntu/contactbook'
+set :deploy_to, '/home/deploy/contactbook'
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml config/secrets.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
